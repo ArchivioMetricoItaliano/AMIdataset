bd680bfb8feb143204046268460d20ba4e1a36cb
giacomounivr <giacomo.marchioro@univr.it>
2022-06-22 18:48:46 -0400

    Corrette descrizioni.

--- readjustcsv.py
+++ readjustcsv.py
@@ -12,8 +12,11 @@ for ind,trascrizione in enumerate(trascrizioniAMI):
         conformsTo = next(g)
         identifier = next(g)
         description = next(g)
-        author = next(g)
+        desc = list(csv.reader([description]))[0]
+        author = next(g) 
+        aut = list(csv.reader([author]))[0][1]
         title = next(g)
+        tit = list(csv.reader([title]))[0][1]
         date = next(g)
         source1 = next(g)
         source2 = next(g)
@@ -22,25 +25,27 @@ for ind,trascrizione in enumerate(trascrizioniAMI):
         contributor = next(g)
         provenance = next(g)
         publisher = next(g)
-        pb = "#dc:publisher,Gruppo Padovano di Stilistica,,,# l'ente responsabile della schedatura,,,,,,,,,,,,,,\n"
-        if publisher != pb:
-            with open(os.path.join("dataset2",trascrizione),'w') as f2:
-                f2.write(DEFINITIONS)
-                f2.write(conformsTo)
-                f2.write(identifier)
-                f2.write(description)
-                f2.write(author)
-                f2.write(title)
-                f2.write(date)
-                f2.write(source1)
-                f2.write(source2)
-                f2.write(source3)
-                f2.write(creator)
-                f2.write(contributor)
-                f2.write(publisher)
-                f2.write(provenance)
-                for i in g:
-                    f2.write(i)  
+        if desc[1] == "Fellowship Marco Praloran 2013":
+            desc[1] = f"Trascrizione e analisi dei versi del componimento {tit} di {aut}. Fellowship Marco Praloran 2013. "
+        else:
+             desc[1] = f"Trascrizione e analisi dei versi del componimento {tit} di {aut}. Riadattato dall'archivio AMI originale. "
+        with open(os.path.join("dataset2",trascrizione),'w') as f2:
+            f2.write(DEFINITIONS)
+            f2.write(conformsTo)
+            f2.write(identifier)
+            f2.write(",".join(desc)+"\n")
+            f2.write(author)
+            f2.write(title)
+            f2.write(date)
+            f2.write(source1)
+            f2.write(source2)
+            f2.write(source3)
+            f2.write(creator)
+            f2.write(contributor)
+            f2.write(provenance)
+            f2.write(publisher)
+            for i in g:
+                f2.write(i)  
 
                 
 
